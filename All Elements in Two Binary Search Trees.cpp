bd62273237d65ec6a00b32a72e3bd949de1153bd
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-11-12 17:13:22 +0530

    Update All Elements in Two Binary Search Trees.cpp

--- All Elements in Two Binary Search Trees.cpp
+++ All Elements in Two Binary Search Trees.cpp
@@ -26,3 +26,50 @@ public:
         return v1;
     }
 };
+
+                            |
+                            |
+                            |
+                            |
+                           \_/ 
+                        OPTIMISATION
+                        
+//Runtime: 192 ms, faster than 85.61% of C++ online submissions for All Elements in Two Binary Search Trees.
+//Memory Usage: 88.2 MB, less than 5.02% of C++ online submissions for All Elements in Two Binary Search Trees.                        
+class Solution {
+    void inorder(TreeNode* root, vector<int>& v) {
+        if(!root){
+            return;
+        }
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> v1, v2,v3;
+        inorder(root1,v1),
+        inorder(root2,v2);
+        int i=0, j=0, 
+        n1=v1.size(),
+        n2=v2.size();
+        while(i<n1 && j<n2)
+            if(v1[i] < v2[j])
+            {
+                v3.push_back(v1[i++]);
+            }
+            else 
+            {
+                v3.push_back(v2[j++]);
+            }
+        while(i<n1) 
+        {
+           v3.push_back(v1[i++]);
+        }
+        while(j<n2)
+        {
+            v3.push_back(v2[j++]);
+        }
+        return v3;
+    }
+};
