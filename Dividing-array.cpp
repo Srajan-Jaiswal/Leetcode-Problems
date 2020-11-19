87bca0306568356c628fbde31197b76768310140
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-11-19 19:15:47 +0530

    Create Dividing-array.cpp

---
+++ Dividing-array.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fr(i, a, n) for (int i = a; i < n; i++)
+#define fre(i, a, b) for (int i = a; i <= b; i++)
+#define endl '\n'
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define case cout << "Case " << t++ << ": ";
+ 
+int a[100100];
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        int  mx=0,mn=0;
+        sort(a,a+n);
+        for(int i=0;i<n/2;i++)
+        {
+            mx+=(a[i+n/2]-a[i]);
+            mn+=(a[2*i+1]-a[2*i]);
+        }
+        cout<<mn<<" "<<mx<<endl;
+    }
+    return 0;
+}
