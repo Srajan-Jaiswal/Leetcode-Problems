7466a71c568e585837dbdceb8be4e8abd69cb207
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-12-08 23:51:33 +0530

    Create Add Strings.cpp

---
+++ Add Strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        reverse(num1.begin(),num1.end());
+        reverse(num2.begin(),num2.end());
+        int n=max(num1.size(),num2.size());
+        int carry=0;
+        string sum;
+        for(int i=0;i<n;i++){
+            if (i<num1.size()) carry+=num1[i]-'0';
+            if (i<num2.size()) carry+=num2[i]-'0';
+            sum.push_back(carry%10+'0');
+            carry/=10;
+        }
+        if (carry) sum.push_back('1');
+        reverse(sum.begin(),sum.end());
+        return sum;
+    }
+};
