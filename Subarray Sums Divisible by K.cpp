7c1081cd86547c29cdac006fe692e70c9b6357f1
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2021-01-14 01:20:18 +0530

    Update Subarray Sums Divisible by K.cpp

--- Subarray Sums Divisible by K.cpp
+++ Subarray Sums Divisible by K.cpp
@@ -1,5 +1,34 @@
-// O(N2) in-efficient approach. 
+// Runtime: 64 ms, faster than 98.64% of C++ online submissions for Subarray Sums Divisible by K.
+// Memory Usage: 31.7 MB, less than 67.25% of C++ online submissions for Subarray Sums Divisible by K.
 
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+        int cnt=0,sum=0,rem,rep;
+        unordered_map<int,int> m;
+        m[0]++; 
+        for(int i=0;i<A.size();i++)
+        {
+            sum += A[i];
+            rem = sum%K;
+            if(rem < 0){
+                rem += K;
+            }
+            if(m.find(rem) != m.end()){
+               rep = m[rem];  
+               cnt+= rep;
+            }
+            m[rem]++;
+         } 
+        /*for(auto it : m)
+        {
+            cout<<it.first<<"-->"<<it.second<<endl;
+        }*/
+        return cnt;
+    }
+};
+
+// O(N2) in-efficient approach. 
 class Solution {
 public:
     int subarraysDivByK(vector<int>& A, int K) {
