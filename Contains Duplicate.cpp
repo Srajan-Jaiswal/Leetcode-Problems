4bf1e0359f88fe25eb5a00b2514a1307d33a3202
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2021-01-15 00:19:49 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (auto x: nums){
+            if (s.find(x) != s.end()){
+                return true;
+            }
+            s.insert(x);
+        }
+        return false;
+    }
+};
