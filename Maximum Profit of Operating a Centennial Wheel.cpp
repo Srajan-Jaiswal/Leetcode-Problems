984eb97a5aebc78acc839964dba4bc488c4a69a9
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2021-02-14 21:07:58 +0530

    Update Maximum Profit of Operating a Centennial Wheel.cpp

--- Maximum Profit of Operating a Centennial Wheel.cpp
+++ Maximum Profit of Operating a Centennial Wheel.cpp
@@ -1,43 +1,72 @@
 // Runtime: 92 ms, faster than 99.20% of C++ online submissions for Maximum Profit of Operating a Centennial Wheel.
 // Memory Usage: 81.5 MB, less than 60.00% of C++ online submissions for Maximum Profit of Operating a Centennial Wheel.
 
-
 class Solution {
 public:
-    int minOperationsMaxProfit(vector<int>& customers, int boardingCost, int runningCost) 
-	{
-		if(boardingCost * 4 < runningCost)
-			return -1;
-
-    	long long int res = -1, max_profit = 0, curr_profit = 0;
-    	for(int i = 0; i < customers.size(); i++)
-    	{
-    		if(i > 0)
-    			customers[i] += max(0, customers[i - 1] - 4);
-    		
-    		int boarding = min(4, customers[i]);
-    		
-    		int profit = boarding * boardingCost - runningCost;
-    		curr_profit += profit;
-    		if(curr_profit > max_profit)
-    		{
-    			max_profit = curr_profit;
-    			res = i + 1;
-    		}
-    	}
-
-    	
-    	customers.back() -= 4;
-
-    	if(customers.back() > 0)
-    		curr_profit += (boardingCost * (customers.back() / 4) * 4) - (customers.back() / 4) * runningCost;
-
-    	if(curr_profit > max_profit)
-    		res = customers.size() + customers.back() / 4;
-
-    	if((customers.back() % 4 * boardingCost) > runningCost)
-    		res = customers.size() + customers.back() / 4 + 1; 
-
-    	return res;
+    int minOperationsMaxProfit(vector<int>& customers, int bc, int rc) {
+        int i=0;
+        int r=1;
+        int cus = 0;
+        int rem = 0;
+        int  ans= INT_MIN;
+        int sub;
+        unordered_map<int,int> um; 
+        while(i<=customers.size() || rem>0){
+            sub = rc * r;
+            if(i>=customers.size()){
+                if(rem >= 4){
+                cus += 4;
+                rem = rem-4;
+                ans = cus*bc-sub;
+                    um[ans]=i+1;
+                    //cout<<ans<<endl;
+                    i++;
+            }
+                else{
+                 cus += rem;
+                    //cout<<cus<<endl;
+                 ans = (cus*bc-sub);
+                   // cout<<ans;
+                    um[ans]=i;
+                    rem=0;
+                     i++;
+            }
+            r++;
+              
+            }
+            else{
+            
+            customers[i] += rem;
+            //cout<< customers[i]<<endl;
+            if(customers[i] >= 4){
+                cus += 4;
+                //cout<<cus<<endl;
+                ans = cus*bc-sub;
+                um[ans]=i;
+               //cout<<ans<<endl;
+                rem = customers[i]-4;
+            }
+            else{
+                cout<<"srajan"<<endl;
+                 cus += customers[i];
+                 ans = cus*bc-sub;
+                 um[ans]=i;
+                // cout<<ans<<endl;
+            }
+                 i++;
+                r++;
+            }
+         
+        }
+        for(auto u: um){
+            ans = max(ans,u.first);
+            cout<< u.first<<"->"<<u.second<<endl;
+        }
+        if(ans<0){
+            return -1;
+        }
+        
+        return um[ans];
+        
     }
 };
