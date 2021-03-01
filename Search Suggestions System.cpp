d4f6307f1f7b9697f4480de42a2f18d133cfa0f4
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2021-03-01 12:37:12 +0530

    Update Search Suggestions System.cpp

--- Search Suggestions System.cpp
+++ Search Suggestions System.cpp
@@ -6,7 +6,7 @@ public:
     Solution(){
         ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     }
-    bool flag=1;
+    /*
     set<string> s;
     vector<vector<string>> ans;
     void func(string str){
@@ -32,6 +32,25 @@ public:
         for(int i=0;i<searchWord.length();i++){
             res+=searchWord[i];
             func(res);
+        }
+        return ans;
+    */
+    
+    vector<string> func(vector<string>& mini, string str) {
+        auto it1 = lower_bound(mini.begin(),mini.end(),str);
+        str[str.size()-1]++;
+        auto it2 = lower_bound(mini.begin(),mini.end(),str);
+        if(it2-it1 > 3) it2 = it1+3;
+        return vector<string>(it1,it2);
+    }
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        string res = "";
+        vector<vector<string>> ans;
+        sort(products.begin(),products.end());
+        for(int i=0;i<searchWord.length();i++){
+            res+=searchWord[i];
+        ans.push_back(func(products,res));
         }
         return ans;
     }
